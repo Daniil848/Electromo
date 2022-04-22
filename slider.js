d7fc04eac1730555eecf66c7db1bddf0589b2642
Daniil848 <ladygindaniil848@gmail.com>
2022-04-22 21:35:13 +0300

    fixed

--- slider.js
+++ slider.js
@@ -1,27 +1,44 @@
-let slide = 0;
+let slideIndex = 0;
 
 const slides = document.getElementsByClassName("main__slider-item");
 
 document.getElementsByClassName("slider__arrow-left")[0].addEventListener("click", function(e) {
     e.preventDefault()
 
-    if (slide > 0) {
-        slides[slide].classList.remove("main__slider-item--active");
-        slide--;
-        slides[slide].classList.add("main__slider-item--active");
-        console.log(slide) 
-    } else {
-        slide = 0;
-    }    
+    if (slideIndex > 0) {
+        slides[slideIndex].classList.remove("main__slider-item--active");
+        slideIndex--;
+        slides[slideIndex].classList.add("main__slider-item--active"); 
+
+        clearInterval(interval)
+    } 
+    
+    
 });
 
 document.getElementsByClassName("slider__arrow-right")[0].addEventListener("click", function(e) {
     e.preventDefault()
 
-    if (slide < (slides.length - 1)) {
-        slides[slide].classList.remove("main__slider-item--active");
-        slide++;
-        slides[slide].classList.add("main__slider-item--active");
-        console.log(slide)
+    if (slideIndex < (slides.length - 1)) {
+        slides[slideIndex].classList.remove("main__slider-item--active");
+        slideIndex++;
+        slides[slideIndex].classList.add("main__slider-item--active");
+ 
+        clearInterval(interval)
     }    
 });
+
+function sliderSwipAuto() {
+
+    if ( slideIndex === (slides.length - 1) ) {
+    slides[slideIndex].classList.remove('main__slider-item--active');
+    slideIndex = 0
+    slides[slideIndex].classList.add('main__slider-item--active');
+    } else {
+    slides[slideIndex].classList.remove('main__slider-item--active');
+    slideIndex++;
+    slides[slideIndex].classList.add('main__slider-item--active');
+    }
+}
+
+const interval = setInterval(sliderSwipAuto, 4000);
